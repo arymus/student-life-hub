e41a3907414c63dfe7b1d1048698dceee9efaa6d
arymus <arymusr@gmail.com>
2026-02-10 16:47:10 -0500

    Added more CSS + JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// --- FORMS ---
+const forms = document.querySelectorAll("form");
+
+const basicInfoForm = document.querySelector("#basic-info > form");
+const moreInfoForm = document.querySelector("#more-info > form");
+const scheduleForm = document.querySelector("#schedule > form");
+const toDoForm = document.querySelector("#tasks > form");
